bd8ea916b60dbcfa63e0c966efd52b4706ffdb36
shivareddy <skreddy2428@gmail.com>
2022-10-07 18:10:46 +0530

    new post commit

--- blogBackend/src/main/java/com/backend/blogBackend/services/PostService.java
+++
@@ -1,8 +0,0 @@
-package com.backend.blogBackend.services;
-
-import com.backend.blogBackend.VO.PostVo;
-
-public interface PostService {
-PostVo createPost(PostVo postVo);
-void deletePost(Integer postId);
-}
