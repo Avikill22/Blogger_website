a296484414378f99e8acb91b3a7d9732a1614705
Avinash Gupta <guptaavi22041998@gmail.com>
2022-10-10 00:02:23 +0530

    making some changes in post API.

--- blogBackend/src/main/java/com/backend/blogBackend/Entity/Post.java
+++ blogBackend/src/main/java/com/backend/blogBackend/Entity/Post.java
@@ -1,5 +1,7 @@
 package com.backend.blogBackend.Entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,60 +18,85 @@ public class Post {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int postId;
+	private Integer postId;
+	
+	@Column(name="description")
 	private String description;
+	
 	@Column(length = 200000)
 	private byte[] imgByte;
 	
+	@Column(name = "post_title",length = 100, nullable = false)
 	private String title;
-	private boolean active;
 	
-	 @ManyToOne
-	    @JoinColumn(name = "userId", insertable = false, updatable = false, referencedColumnName = "userId")
-	    private User user;
+	@Column(name="date")
+	private Date addedDate;
+	
+	@Column(name="active_flag")
+	private Boolean active;
+	
+	private User user;
 	
 	public Post() {
+		super();
 		
 	}
-	
-	public User getUser() {
-		return user;
+
+	public Post(Integer postId, String description, byte[] imgByte, String title, Date addedDate, Boolean active) {
+		super();
+		this.postId = postId;
+		this.description = description;
+		this.imgByte = imgByte;
+		this.title = title;
+		this.addedDate = addedDate;
+		this.active = active;
 	}
 
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public int getPostId() {
+	public Integer getPostId() {
 		return postId;
 	}
-	public void setPostId(int postId) {
+
+	public void setPostId(Integer postId) {
 		this.postId = postId;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public byte[] getImgByte() {
 		return imgByte;
 	}
+
 	public void setImgByte(byte[] imgByte) {
 		this.imgByte = imgByte;
 	}
-	
-	
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public boolean isActive() {
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public Boolean getActive() {
 		return active;
 	}
-	public void setActive(boolean active) {
+
+	public void setActive(Boolean active) {
 		this.active = active;
 	}
 	
